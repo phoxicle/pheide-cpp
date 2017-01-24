afca22d67f4e646ee7777912e054a74bfbd37eab
Chris Gerpheide <christine.ger@phei.de>
2017-01-24 09:08:42 +0100

    Simple file printing HTML content for CGI

---
+++ web.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main () {
+    
+   cout << "Content-type:text/html\r\n\r\n";
+   cout << "<html>\n";
+   cout << "<head>\n";
+   cout << "<title>Hello CGI</title>\n";
+   cout << "</head>\n";
+   cout << "<body>\n";
+   cout << "<h2>Hello aoeu </h2>\n";
+   cout << "</body>\n";
+   cout << "</html>\n";
+   
+   return 0;
+}
