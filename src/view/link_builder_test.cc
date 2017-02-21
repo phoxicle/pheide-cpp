9082e0eabfc1b8b0e36ec0260a433e07a7f71f63
Chris Gerpheide <christine.ger@phei.de>
2017-02-21 09:26:53 +0100

    Real link builder tests

--- src/view/link_builder_test.cc
+++ src/view/link_builder_test.cc
@@ -1,7 +1,44 @@
 #include "link_builder.h"
 #include "gtest/gtest.h"
 
-TEST(RouterTest, Positive) {
-  EXPECT_EQ(1, 1);
+TEST(LinkBuilderTest, AllParams) {
+	// GIVEN
+	auto builder = pheide::view::LinkBuilder();
+
+	// WHEN
+	std::string link = builder
+		.withAction("foo")
+		.withPageId(1)
+		.withTabId(2)
+		.build();
+
+	// THEN
+	EXPECT_EQ("?action=foo&page_id=1&tab_id=2", link);
+}
+
+TEST(LinkBuilderTest, NoTabId) {
+	// GIVEN
+	auto builder = pheide::view::LinkBuilder();
+
+	// WHEN
+	std::string link = builder
+		.withAction("foo")
+		.withPageId(1)
+		.build();
+
+	// THEN
+	EXPECT_EQ("?action=foo&page_id=1", link);
+}
+
+TEST(LinkBuilderTest, EmptyParams) {
+	// GIVEN
+	auto builder = pheide::view::LinkBuilder();
+
+	// WHEN
+	std::string link = builder
+		.build();
+
+	// THEN
+	EXPECT_EQ("?", link);
 }
 
